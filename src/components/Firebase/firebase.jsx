53162303177dc1e9c8f7b7fe0783ddc8f593d750
Adiomojeed <adio.mojeed@gmail.com>
2020-04-29 13:39:19 +0100

    Installed recompense to handle HOC nesting

--- src/components/Firebase/firebase.jsx
+++ src/components/Firebase/firebase.jsx
@@ -1,6 +1,7 @@
 /** @format */
 
-import app from 'firebase/app'
+import app from 'firebase/app';
+import 'firebase/auth'
 
 const config = {
 	apiKey: "AIzaSyCk1pr_Wkc4_pbFrBXkXyMTyQ2h80adlDY",
@@ -14,6 +15,37 @@ const config = {
 class Firebase {
 	constructor () {
 		app.initializeApp(config)
+		this.auth = app.auth()
+		this.doCreateUserWithEmailAndPassword = this.doCreateUserWithEmailAndPassword.bind(this)
+		this.doSignInWithEmailAndPassword = this.doSignInWithEmailAndPassword.bind(this)
+		this.doSignOut = this.doSignOut.bind(this)
+		this.doResetPassword = this.doResetPassword.bind(this)
+		this.doChangePassword = this.doResetPassword.bind(this)
+	}
+
+	// Sign Up function
+	doCreateUserWithEmailAndPassword(email, password)  {
+		this.auth.createUserWithEmailAndPassword(email, password)
+	}
+
+	// Sign In function
+	doSignInWithEmailAndPassword(email, password)  {
+		this.auth.signInWithEmailAndPassword(email, password)
+	}
+
+	// Sign Out function
+	doSignOut() {
+		this.auth.signOut()
+	}
+
+	// Reset Password function
+	doResetPassword(email) {
+		this.auth.sendPasswordResetEmail(email)
+	}
+
+	// Change Password function
+	doChangePassword(password) {
+		this.auth.currentUser.updatePassword(password)
 	}
 }
 
